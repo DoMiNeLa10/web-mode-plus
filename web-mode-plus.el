55023519ee45b1f663f15e6300feb619ef6eae53
DoMiNeLa10 <retrogamingdomi@wp.pl>
2019-04-19 14:46:26 +0200

    add package-requires

--- web-mode-plus.el
+++ web-mode-plus.el
@@ -2,6 +2,8 @@
 
 ;; Copyright (C) 2017 DoMiNeLa10
 
+;; Package-Requires: (web-mode)
+
 ;; This program is free software; you can redistribute it and/or
 ;; modify it under the terms of the GNU General Public License
 ;; as published by the Free Software Foundation; either version 2
